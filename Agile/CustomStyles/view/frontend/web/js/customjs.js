21be797dd092e6d87a53aada1bc335d075af94ff
Agile Cyber Management Solutions <support@agilecyber.com>
2023-10-05 11:15:32 +0530

    Update customjs.js

--- Agile/CustomStyles/view/frontend/web/js/customjs.js
+++ Agile/CustomStyles/view/frontend/web/js/customjs.js
@@ -1,13 +1,6 @@
 require(["jquery"],function($){
-    var currentURL = window.location.href;
-    if (currentURL){
-        var hash_shipping = window.location.hash;
-        // console.log(hash_shipping);
-        if (hash_shipping == '#shipping'){
-            localStorage.setItem('page-info', 'shipping');
-        }
-        if (hash_shipping == '#payment'){
-            localStorage.setItem('page-info', 'payment');
-        }
+    
+        console.log("custom js");
+        
     }
 });
